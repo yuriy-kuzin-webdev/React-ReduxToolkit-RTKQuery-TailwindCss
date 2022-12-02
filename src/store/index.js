72c8c3a6a14c348ee0f57b238b0487cf09256618
yuriykuzinpd <yuriy.kuzin.pd@gmail.com>
2022-12-02 22:58:16 +0200

    registered api

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { githubApi } from "./github/github.api";
 
 export const store = configureStore({
-    reducer: {}
+    reducer: {
+        [githubApi.reducerPath]: githubApi.reducer
+    }
 })
