59f068bd382a993615353d7cea8c83f5c70d7ecb
yuriykuzinpd <yuriy.kuzin.pd@gmail.com>
2022-12-02 22:42:40 +0200

    added navigation

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,17 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import FavoritesPage from "./pages/FavoritesPage";
 import HomePage from "./pages/HomePage";
+import Navigation from "./components/Navigation";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/favorites" element={<FavoritesPage />} />
-    </Routes>
+    <>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/favorites" element={<FavoritesPage />} />
+      </Routes>
+    </>
   );
 }
 
