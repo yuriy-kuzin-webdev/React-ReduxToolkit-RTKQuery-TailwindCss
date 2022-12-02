0affcc4eec27820bfb0051d73d8552a6249f9c31
yuriykuzinpd <yuriy.kuzin.pd@gmail.com>
2022-12-02 22:35:35 +0200

    added routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
-import React from 'react';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import FavoritesPage from "./pages/FavoritesPage";
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
-    <></>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/favorites" element={<FavoritesPage />} />
+    </Routes>
   );
 }
 
