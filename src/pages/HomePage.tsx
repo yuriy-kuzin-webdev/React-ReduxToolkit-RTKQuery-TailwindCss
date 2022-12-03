f4059c7b74adbc63d79e1b8d14db3cb284a04663
yuriykuzinpd <yuriy.kuzin.pd@gmail.com>
2022-12-03 16:24:23 +0200

    updated homepage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,11 +1,17 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { useDebounce } from "../hooks/debounce";
 import { useSearchUsersQuery } from "../store/github/github.api";
 
 function HomePage() {
-  const { isLoading, isError, data } = useSearchUsersQuery("kuzin");
   const [search, setSearch] = useState("");
-  console.log(data);
-
+  const [dropdown, setDropdown] = useState(false);
+  const debounced = useDebounce(search);
+  const { isLoading, isError, data } = useSearchUsersQuery(debounced, {
+    skip: debounced.length < 3,
+  });
+  useEffect(() => {
+    setDropdown(debounced.length > 3 && data?.length! > 0);
+  }, [debounced, data]);
   return (
     <div className="flex justify-center pt-10 mx-auto h-screen w-screen">
       {isError && (
@@ -20,7 +26,19 @@ function HomePage() {
           onChange={(e) => setSearch(e.target.value)}
         />
 
-        <div className="absolute top-[42px] left-0 right-0 max-h-[200px] shadow-md bg-white"></div>
+        {dropdown && (
+          <ul className="list-none absolute top-[42px] left-0 right-0 max-h-[200px] shadow-md bg-white overflow-y-scroll">
+            {isLoading && <p className="text-center">Loading...</p>}
+            {data?.map((user) => (
+              <li
+                key={user.id}
+                className="py-2 px-4 hover:bg-gray-500 hover:text-white transition-colors cursor-pointer"
+              >
+                {user.login}
+              </li>
+            ))}
+          </ul>
+        )}
       </div>
     </div>
   );
