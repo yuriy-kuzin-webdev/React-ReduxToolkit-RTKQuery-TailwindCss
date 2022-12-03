bb5be89c45531367743b6ffda24149da11bca5b0
yuriykuzinpd <yuriy.kuzin.pd@gmail.com>
2022-12-03 18:49:25 +0200

    added delete button

--- src/components/RepoCard.tsx
+++ src/components/RepoCard.tsx
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import { useActions } from "../hooks/actions";
+import { useAppSelector } from "../hooks/redux";
 import { IRepo } from "../types/types";
 
 function RepoCard({ repo }: { repo: IRepo }) {
-  const { addFavorite } = useActions();
+  const { addFavorite, removeFavorite } = useActions();
+  const { favorites } = useAppSelector((state) => state.github);
+
+  const [isFavorite, setIsFavorite] = useState(
+    favorites.includes(repo.html_url)
+  );
 
   const addToFavorite = (event: React.MouseEvent<HTMLButtonElement>) => {
     event.preventDefault();
     addFavorite(repo.html_url);
+    setIsFavorite(true);
+  };
+  const removeFromFavorites = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+    removeFavorite(repo.html_url);
+    setIsFavorite(false);
   };
   return (
     <div className="border py-3 px-5 rounded mb-2 hover:shadow-md hover:bg-gray-100 transition-all">
@@ -18,12 +30,22 @@ function RepoCard({ repo }: { repo: IRepo }) {
           Watchers: <span className="font-bold">{repo.watchers}</span>
         </p>
         <p className="text-sm font-thin">{repo?.description}</p>
-        <button
-          className="py-2 px-4 bg-yellow-400 rounded hover:shadow-md transition-all"
-          onClick={addToFavorite}
-        >
-          Add
-        </button>
+        {!isFavorite && (
+          <button
+            className="py-2 px-4 bg-yellow-400 mr-2 rounded hover:shadow-md transition-all"
+            onClick={addToFavorite}
+          >
+            Add
+          </button>
+        )}
+        {isFavorite && (
+          <button
+            className="py-2 px-4 bg-red-400 rounded hover:shadow-md transition-all"
+            onClick={removeFromFavorites}
+          >
+            Remove
+          </button>
+        )}
       </a>
     </div>
   );
