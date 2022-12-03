22475a3a205bca6b0e668191feca5742a0edcd3d
yuriykuzinpd <yuriy.kuzin.pd@gmail.com>
2022-12-03 18:39:35 +0200

    added simple markup to favorites page

--- src/pages/FavoritesPage.tsx
+++ src/pages/FavoritesPage.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+import { useAppSelector } from "../hooks/redux";
 
 function FavoritesPage() {
+  const { favorites } = useAppSelector((state) => state.github);
+  if (favorites.length === 0) return <p className="text-center">No items.</p>;
+
   return (
-    <div>FavoritesPage</div>
-  )
+    <div className="flex justify-center pt-10 mx-auto h-screen w-screen">
+      <ul className="list-none">
+        {favorites.map((fav) => (
+          <li key={fav}>
+            <a href={fav} target="_blank" rel="noreferrer">
+              {fav}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
-export default FavoritesPage
+export default FavoritesPage;
