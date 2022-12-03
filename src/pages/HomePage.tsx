9e99eed000462ee6f2182be642166234ab06fef2
yuriykuzinpd <yuriy.kuzin.pd@gmail.com>
2022-12-03 16:49:28 +0200

    added repo list

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,18 +1,31 @@
 import React, { useEffect, useState } from "react";
 import { useDebounce } from "../hooks/debounce";
-import { useSearchUsersQuery } from "../store/github/github.api";
+import {
+  useLazyGetUserReposQuery,
+  useSearchUsersQuery,
+} from "../store/github/github.api";
 
 function HomePage() {
   const [search, setSearch] = useState("");
   const [dropdown, setDropdown] = useState(false);
+
   const debounced = useDebounce(search);
+
   const { isLoading, isError, data } = useSearchUsersQuery(debounced, {
     skip: debounced.length < 3,
-    refetchOnFocus: true
+    refetchOnFocus: true,
   });
+  const [fetchRepos, { isLoading: areReposLoading, data: repos }] =
+    useLazyGetUserReposQuery();
+
   useEffect(() => {
     setDropdown(debounced.length > 3 && data?.length! > 0);
   }, [debounced, data]);
+
+  const onListItemClick = (userLogin: string) => {
+    fetchRepos(userLogin);
+  };
+
   return (
     <div className="flex justify-center pt-10 mx-auto h-screen w-screen">
       {isError && (
@@ -34,12 +47,23 @@ function HomePage() {
               <li
                 key={user.id}
                 className="py-2 px-4 hover:bg-gray-500 hover:text-white transition-colors cursor-pointer"
+                onClick={() => {
+                  onListItemClick(user.login);
+                }}
               >
                 {user.login}
               </li>
             ))}
           </ul>
         )}
+        <div className="container">
+          {areReposLoading && (
+            <p className="text-center">Repos are loading...</p>
+          )}
+          {repos?.map((repo) => (
+            <p>{repo.url}</p>
+          ))}
+        </div>
       </div>
     </div>
   );
