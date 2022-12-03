01a0d3ea4c4e3eba967ca8c7a3539bdadf0132ab
yuriykuzinpd <yuriy.kuzin.pd@gmail.com>
2022-12-03 17:17:54 +0200

    updated store

--- src/store/github/github.api.ts
+++ src/store/github/github.api.ts
@@ -1,5 +1,4 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-import { url } from "inspector";
 import { IRepo, IUser, ServerResponse } from "../../types/types";
 
 export const githubApi = createApi({
