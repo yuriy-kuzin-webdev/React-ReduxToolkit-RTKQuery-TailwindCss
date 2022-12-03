bd7d388f5b6d49de525d6bdb1e7088ee317f50bb
yuriykuzinpd <yuriy.kuzin.pd@gmail.com>
2022-12-03 18:29:32 +0200

    added button for adding favorites

--- src/components/RepoCard.tsx
+++ src/components/RepoCard.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import { useActions } from "../hooks/actions";
 import { IRepo } from "../types/types";
 
 function RepoCard({ repo }: { repo: IRepo }) {
+  const { addFavorite } = useActions();
+
+  const addToFavorite = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+    addFavorite(repo.html_url);
+  };
   return (
     <div className="border py-3 px-5 rounded mb-2 hover:shadow-md hover:bg-gray-100 transition-all">
       <a href={repo.html_url} target="_blank">
@@ -11,6 +18,12 @@ function RepoCard({ repo }: { repo: IRepo }) {
           Watchers: <span className="font-bold">{repo.watchers}</span>
         </p>
         <p className="text-sm font-thin">{repo?.description}</p>
+        <button
+          className="py-2 px-4 bg-yellow-400 rounded hover:shadow-md transition-all"
+          onClick={addToFavorite}
+        >
+          Add
+        </button>
       </a>
     </div>
   );
